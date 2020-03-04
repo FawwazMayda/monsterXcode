7db7e20932d7f4b4bc445db8763cab86597c3b0a
fawwazmayda <maydafawwaz@gmail.com>
2020-03-04 14:05:58 +0700

    Initial Commit

---
+++ monsterSV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  monsterSV
+//
+//  Created by Muhammad Fawwaz Mayda on 04/03/20.
+//  Copyright © 2020 Muhammad Fawwaz Mayda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
